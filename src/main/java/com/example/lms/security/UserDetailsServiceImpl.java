28ee59e010e0d2e4ffd6c128f1e0602abb83805b
kbaduwal <khakendra1999@gmail.com>
2025-01-20 15:36:21 +0545

    User details service Impl

--- src/main/java/com/example/lms/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/lms/security/UserDetailsServiceImpl.java
@@ -1,4 +1,25 @@
 package com.example.lms.security;
 
-public class UserDetailsServiceImpl {
+import com.example.lms.entity.User;
+import com.example.lms.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
 }
