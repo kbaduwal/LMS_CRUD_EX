edad69137169eb9e932d50bb496dafb5921d1219
kbaduwal <khakendra1999@gmail.com>
2025-01-21 14:37:54 +0545

    creating instances of roles based on role names.

---
+++ src/main/java/com/example/lms/factories/RoleFactory.java
@@ -0,0 +1,31 @@
+package com.example.lms.factories;
+
+import com.example.lms.entity.Role;
+import com.example.lms.enums.UserRole;
+import com.example.lms.exception.RoleNotFoundException;
+import com.example.lms.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleFactory {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    public Role getInstance(String role) throws RoleNotFoundException {
+        switch (role){
+            case "librarian" -> {
+                return roleRepository.findByName(UserRole.LIBRARIAN);
+            }
+            case "student" -> {
+                return roleRepository.findByName(UserRole.STUDENT);
+            }
+            case "teacher" -> {
+                return roleRepository.findByName(UserRole.TEACHER);
+            }
+            default -> throw new RoleNotFoundException("No role found for role: " + role);
+        }
+
+    }
+}
