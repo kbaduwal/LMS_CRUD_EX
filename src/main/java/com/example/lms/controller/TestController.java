e8288f9061842e798aae6ecff8b54ee92cb7626e
kbaduwal <khakendra1999@gmail.com>
2025-01-22 16:35:41 +0545

    Role based authentication.

--- src/main/java/com/example/lms/controller/TestController.java
+++ src/main/java/com/example/lms/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.lms.controller;
 import com.example.lms.dto.ApiResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,57 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
-    @GetMapping("/")
-    public ResponseEntity<ApiResponseDto<?>> Test() {
+    //Only user with "STUDENT" role can access this end point
+    @GetMapping("/student")
+    @PreAuthorize("hasRole('STUDENT')")
+    public ResponseEntity<ApiResponseDto<?>> StudentDashboard() {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new ApiResponseDto<>(true,"Lets learn spring Security with Jwt",null));
+                .body(ApiResponseDto.builder()
+                        .isSuccess(true)
+                        .message("Student Dashboard")
+                        .build()
+                );
     }
+
+    //Only user with "TEACHER" role can access this end point
+    @GetMapping("/teacher")
+    @PreAuthorize("hasRole('TEACHER')")
+    public ResponseEntity<ApiResponseDto<?>> TeacherDashboard() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(
+                        ApiResponseDto.builder()
+                                .isSuccess(true)
+                                .message("Teacher Dashboard")
+                                .build()
+                );
+    }
+
+    //Only with user "LIBRARIAN" role can access this end point
+    @GetMapping("/librarian")
+    @PreAuthorize("hasRole('librarian')")
+    public ResponseEntity<ApiResponseDto<?>> LibrarianDashboard() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponseDto.builder()
+                        .isSuccess(true)
+                        .message("Librarian Dashboard")
+                        .build()
+                );
+    }
+
+    //User with "TEACHER" or "LIBRARIAN" roles can access this end pont
+    @GetMapping("/teacherOrLibrarian")
+    @PreAuthorize("hasRole('TEACHER') or hasRole('LIBRARIAN')")
+    public ResponseEntity<ApiResponseDto<?>> TeacherOrLibrarianContent() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponseDto.builder()
+                        .isSuccess(true)
+                        .message("Teacher or Librarian content.")
+                        .build()
+                );
+    }
+
 }
