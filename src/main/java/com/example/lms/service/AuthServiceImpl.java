c5d6489746eff32410863d052424978d22e42f9a
kbaduwal <khakendra1999@gmail.com>
2025-01-22 13:14:11 +0545

    Added logic for sign in process.

--- src/main/java/com/example/lms/service/AuthServiceImpl.java
+++ src/main/java/com/example/lms/service/AuthServiceImpl.java
@@ -2,20 +2,30 @@ package com.example.lms.service;
 
 import com.example.lms.dto.ApiResponseDto;
 import com.example.lms.dto.SignInRequestDto;
+import com.example.lms.dto.SignInResponseDto;
 import com.example.lms.dto.SignUpRequestDto;
 import com.example.lms.entity.Role;
 import com.example.lms.entity.User;
 import com.example.lms.exception.RoleNotFoundException;
 import com.example.lms.exception.UserAlreadyExistsException;
 import com.example.lms.factories.RoleFactory;
+import com.example.lms.security.UserDetailsImpl;
+import com.example.lms.security.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -28,6 +38,13 @@ public class AuthServiceImpl implements AuthService {
 
     @Autowired
     private RoleFactory roleFactory;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
 
     @Override
     public ResponseEntity<ApiResponseDto<?>> signUpUser(SignUpRequestDto signUpRequestDto)
@@ -52,7 +69,35 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public ResponseEntity<ApiResponseDto<?>> signInUser(SignInRequestDto signInRequestDto) {
-        return null;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(signInRequestDto.getEmail(), signInRequestDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles= userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        SignInResponseDto signInResponseDto = SignInResponseDto.builder()
+                .username(userDetails.getUsername())
+                .email(userDetails.getEmail())
+                .id(userDetails.getId())
+                .token(jwt)
+                .type("Bearer")
+                .roles(roles)
+                .build();
+
+
+        return ResponseEntity.ok(
+                ApiResponseDto.builder()
+                        .isSuccess(true)
+                        .message("Sign in Sucessfull!")
+                        .response(signInResponseDto)
+                        .build()
+        );
     }
 
     private User createUser(SignUpRequestDto signUpRequestDto) throws RoleNotFoundException {
